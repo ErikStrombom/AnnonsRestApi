2f94b20b27aab4b6dcf7ffa6607276a2c79669cb
Thornfalt <46998405+Thornfalt@users.noreply.github.com>
2019-05-21 09:52:47 +0200

    Service skit

---
+++ AnnonsRestApi/Controllers/ServiceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestApi.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        // GET: api/Service
+        public ServiceReference1.Service[] Get()
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+            ServiceReference1.Service[] output = client.GetAllServiceData();
+            return output;
+        }
+
+        // GET: api/Service/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Service
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Service/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Service/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
